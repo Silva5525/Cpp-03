f92c7bab58dd6d598597ae5943f3168092c5d859
Silva5525 <132822583+Silva5525@users.noreply.github.com>
2024-11-26 19:33:13 +0100

    ad FragTrap

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,15 @@
 /*   By: wdegraf <wdegraf@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 16:11:53 by wdegraf           #+#    #+#             */
-/*   Updated: 2024/11/24 14:57:15 by wdegraf          ###   ########.fr       */
+/*   Updated: 2024/11/26 13:46:17 by wdegraf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 /// @brief my little test program for the ClapTrap class.
+/// @note We use "\n" instead of std::endl to prevent flushing the buffer.
+/// It is more efficient and faster.. if needet we could use std:flush. 
 int	main(void)
 {
 	ClapTrap claptrap;
